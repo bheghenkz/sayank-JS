ad3e9fc7f7e1ab243ccdf95cbe246359fa8a0915
fendy <fendysupratno@gmail.com>
2018-07-29 21:38:21 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -36,7 +36,7 @@ const config = {
   LINE_SESSION_NAVER_URL: '/authct/v1/keys/naver',
   LINE_POST_CONTENT_URL: 'https://os.line.naver.jp/talk/m/upload.nhn',
   LINE_POST_CONTENT_URL_2ND: 'https://obs-sg.line-apps.com/talk/m/upload.nhn',
-  X_LINE_APP: 'DESKTOPWIN8.3.0NADYA_TJx64',
+  X_LINE_APP: 'CHROMEOS	1.4.17	NADYA-TJ	x64',
   //X_LINE_APP: 'CHROMEOS	1.4.13	Chrome_OS	1',
   ip: '127.0.0.1',
   version: '0.0.2',
@@ -45,7 +45,7 @@ const config = {
   platform: whichPlatform,
   EMAIL_REGEX: /[^@]+@[^@]+\.[^@]+/,
   Headers: {
-    'User-Agent':'Line/8.3.0'
+    'User-Agent':'Line/2.1.5'
   },
   FILE_DOWNLOAD_LOCATION: '/../download/',
   YT_DL: 'http://www.saveitoffline.com/process/',
